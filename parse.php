0c553c0bce96a9cdd454d3df8f0ecac477d43f35
mrlemon19 <luke.jackob@gmail.com>
2023-02-14 20:04:38 +0100

    feat(parse.php): input handle

--- parse.php
+++ parse.php
@@ -1,5 +1,24 @@
 <?php
 // ipp projects, xlukas18 2023
-echo "ipp";
+
+// input handle
+if ($argc != 2) {
+    echo "Chyba: Nespravny pocet argumentu.\n";
+    exit(10);
+}
+
+if ($argv[1] == "--help") {
+    echo "Skript pro parsovani IPPcode23 zdrojaku do XML reprezentace.\n";
+    exit(0);
+}
+
+$source = fopen($argv[1], "r") or exit (11); // TODO: error message
+
+while ($line = fgets($source)) {
+    echo $line;
+}
+
+fclose($source);
+exit(0);
 
 ?>

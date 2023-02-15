c2ff63bf1a1d708fa49d70d9256932cfb3f9ab01
mrlemon19 <luke.jackob@gmail.com>
2023-02-15 15:03:47 +0100

    add: class for instruction and line nember

--- parse.php
+++ parse.php
@@ -1,6 +1,14 @@
 <?php
 // ipp projects, xlukas18 2023
 
+class Instruction {
+    public $line;           // line number in source code
+    public $opcode;         // opcode
+    public $arg1;           // first argument
+    public $arg2 = null;    // second argument
+    public $arg3 = null;    // third argument
+}
+
 // input handle
 if ($argc != 2) {
     echo "Chyba: Nespravny pocet argumentu.\n";
@@ -13,16 +21,19 @@ if ($argv[1] == "--help") {
 }
 
 $source = fopen($argv[1], "r") or exit (11); // TODO: error message
+$linenumber = 1;
 
 while ($line = fgets($source)) {
     $linearr = explode(" ", $line);
+    echo $linenumber." ";
+    $linenumber++;
     for ($i = 0; $i < count($linearr); $i++) {
-        //echo $linearr[$i]."\n";
         if ($linearr[$i] == "#"){
             break;
         }
         else{
             echo $linearr[$i];
+            
         }
     }
 }

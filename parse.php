8d16317d259a45c400741e4fe96acd8f1f4db9d3
mrlemon19 <luke.jackob@gmail.com>
2023-02-15 18:23:21 +0100

    add: instruction class constructor

--- parse.php
+++ parse.php
@@ -5,8 +5,16 @@ class Instruction {
     public $line;           // line number in source code
     public $opcode;         // opcode
     public $arg1;           // first argument
-    public $arg2 = null;    // second argument
-    public $arg3 = null;    // third argument
+    public $arg2;           // second argument
+    public $arg3;           // third argument
+
+    function __construct($line, $opcode, $arg1, $arg2=null, $arg3=null) {
+        $this->line = $line;
+        $this->opcode = $opcode;
+        $this->arg1 = $arg1;
+        $this->arg2 = $arg2;
+        $this->arg3 = $arg3;
+    }
 }
 
 // input handle
@@ -22,13 +30,17 @@ if ($argv[1] == "--help") {
 
 $source = fopen($argv[1], "r") or exit (11); // TODO: error message
 $linenumber = 1;
+$instarray = array();
 
 while ($line = fgets($source)) {
     $linearr = explode(" ", $line);
     echo $linenumber." ";
     $linenumber++;
+
+    rtrim($linearr[count($linearr)-1], "\n");
     for ($i = 0; $i < count($linearr); $i++) {
-        if ($linearr[$i] == "#"){
+
+        if ($linearr[$i] == "#" or $linearr[$i] == "\n"){
             break;
         }
         else{
